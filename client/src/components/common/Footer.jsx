a7f9d153dd7c9b8f20827a06b10d7596a6a68549
vaishnavi <vaishnavivarma11@gmail.com>
2025-04-07 19:07:21 +0530

    footer

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -5,16 +5,13 @@ function Footer() {
     <footer className="footer bg-dark text-light py-5 mt-5">
       <div className="container">
        
-      <footer style="text-align: center; padding: 1rem 0; font-size: 0.9rem; color: #888;">
-  © 2025 Article Hub | Built by Shivakumar Muthe 
-</footer>
-
+      
       
 
       </div>
       
       <div className="text-center mt-5 border-top pt-3">
-        <p className="footer-alt mb-0 f-14">2025 © TheArticleHub, All Rights Reserved</p>
+        <p className="footer-alt mb-0 f-14">2025 © TheArticleHub, By Shivakumar Muthe </p>
       </div>
     </footer>
   );
