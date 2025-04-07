86b10960f3f23a8a341359eb50edf27d963857cf
vaishnavi <vaishnavivarma11@gmail.com>
2025-04-07 19:05:42 +0530

    for bro

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -5,12 +5,10 @@ function Footer() {
     <footer className="footer bg-dark text-light py-5 mt-5">
       <div className="container">
        
-        <div className="parent">
-            
-            <a href="https://www.linkedin.com/in/vaishnavi-muthe-aa5716351/"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQppeFJp-l1XachmicCVeX-Q45TGIISFZd3pg&s"  className='imgs' alt="" /></a>
-            <a href="https://github.com/vaishnavimuthe6"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt6DXuvit57V1DmjnlLcwst4O-sTL5D37gIQ&s" alt="" className='imgs' /></a>
-            <a href="https://www.instagram.com/vaishnavi_varma_6/"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTokAJBQ3G-hzXhwhAv4R2OeGFwSOTCVfQeqw&s"  className='imgs' alt="" /></a>
-        </div>
+      <footer style="text-align: center; padding: 1rem 0; font-size: 0.9rem; color: #888;">
+  © 2025 Article Hub | Built by Shivakumar Muthe 
+</footer>
+
       
 
       </div>
